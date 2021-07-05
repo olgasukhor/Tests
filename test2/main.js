cd4e3ead9e9dc9f73f33639083dc0e4488f21fcf
olgasukhor <86747528+olgasukhor@users.noreply.github.com>
2021-07-05 12:23:57 +0300

    Add files via upload

--- test2/main.js
+++ test2/main.js
@@ -3,15 +3,14 @@ const URI = 'https://jsonplaceholder.typicode.com/posts';
 const fetchGet = async () => {
     let res = await fetch (URI);
     let result = await res.json();
-    return result;
-    
+    return result;    
 }
+
 // function CheckOdd (){
 //     return (result.id % 2) == 1;
 // } //хотела вынести фильтр в отдельную функцию, но тут завтык. так не работает.
+
 const getArray = result => result.filter(result => (result.id % 2) == 1);
-
-
   
 const fetchOddPosts = () => {
     fetchGet().then(result => getArray(result))
