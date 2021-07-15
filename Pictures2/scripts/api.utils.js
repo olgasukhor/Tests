6aaac51a9d70a44581e94bfa5ed93cbe611a31b8
Oleksandr Voronkov <52102495+oleksandr-voronkov@users.noreply.github.com>
2021-07-15 18:31:05 +0300

    Propses to api.js

--- Pictures2/scripts/api.utils.js
+++ Pictures2/scripts/api.utils.js
@@ -1,7 +1,3 @@
 const UNSPLASH_API_URL = 'https://api.unsplash.com/search/photos?client_id=bppMp0tY0Dhju0fRCrKti7FFGgLcTGcHTA8ngpviabQ'
 
-const fetchPictures = (value) => {
-    return new Promise(resolve => {
-        resolve(fetch(`${UNSPLASH_API_URL}&query=${value}`))
-    });
-}
+const fetchPictures = (value) => fetch(`${UNSPLASH_API_URL}&query=${value}`);
