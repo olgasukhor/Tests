2c0b4fc38e6b179817d7bff92f419ea0452340ef
olgasukhor <86747528+olgasukhor@users.noreply.github.com>
2021-07-17 18:52:25 +0300

    Add files via upload

--- ToDo/scripts/dom.utils.js
+++ ToDo/scripts/dom.utils.js
@@ -1,11 +1,13 @@
-const titleForm = document.querySelector('.form-title');
-const descriptionForm = document.querySelector('.description');
-const out = document.querySelector('.out');
 
 const listenDeleteTodo = (element) => {
     element.addEventListener("click", () => {
         element.parentElement.remove();
     });
+}
+const likeOnclick = (element) => {
+    element.addEventListener('click', () => {
+        element.src = "images/like_favorite_heart_3524.png";
+    })
 }
 const createItem = () => {
     let div = document.createElement('div');
@@ -19,16 +21,23 @@ const createItem = () => {
     title.classList.add('todoTitle');
     text.classList.add('todoText');
 
-    const icon = document.createElement("img");
+    const icon = document.createElement('img');
     icon.src = 'images/pngegg.png';
-    icon.classList.add("deleteBtn");
+    icon.classList.add('deleteBtn');
 
+    const like = document.createElement("img");
+    like.src = 'images/like-48_45157.png';
+    like.classList.add('like');
+
+    div.appendChild(like);
     div.appendChild(icon);
+
     out.appendChild(div);
     titleForm.value = '';
     descriptionForm.value = '';
 
     listenDeleteTodo(icon);
+    likeOnclick(like);
 
     return div;
 }
--- ToDo/scripts/main.js
+++ ToDo/scripts/main.js
@@ -1,2 +1,6 @@
+const titleForm = document.querySelector('.form-title');
+const descriptionForm = document.querySelector('.description');
+const out = document.querySelector('.out');
+
 const btn = document.querySelector('.push')
 btn.addEventListener('click', createItem);
