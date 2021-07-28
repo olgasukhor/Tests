4c6e5b8e506a6a0296052f3393fd819d4a855697
olgasukhor <86747528+olgasukhor@users.noreply.github.com>
2021-07-28 23:39:23 +0300

    Add files via upload

--- Classes/index.js
+++ Classes/index.js
@@ -1,28 +1,27 @@
 // Створіть клас Product, який буде приймати в конструкторі аргументи title, price, description та проставляти їх як властивості класу.Потім, створіть клас PizzaProduct, який буде наслідуватись від класу Product.Додайте метод getPizzaPrice() до класу PizzaProduct, який буде повертати інформацію про продукт у вигляді стрічки.
 
 class Product {
+
     constructor(title, price, description) {
         this.title = title;
         this.price = price;
         this.description = description;
-
     }
 }
 
 class PizzaProduct extends Product {
-    constructor(title, price, description) {
-        super(title, price, description)
-    }
+
     getPizzaPrice() {
-        return "Title: " + this.title + "; " + "price: " + this.price + "; " + "description:" + this.description;
+        return "Title: " + this.title + "; " + "price: " + this.price + "; " + "description: " + this.description + '.';
     }
 }
-let pizza = new PizzaProduct('pizza', '200', 'solerno');
-console.log(pizza.getPizzaPrice());
+
+console.log(new PizzaProduct('pizza', '200', 'solerno').getPizzaPrice());
 
 // Створіть клас Book, який буде працювати аналогічно до класу Product, але в аргумети приймати title, description, author.Другий клас буде називатися AdventureBook та мати метод getAdventureBookInfo().
 
 class Book {
+
     constructor(title, description, author) {
         this.title = title;
         this.description = description;
@@ -31,12 +30,10 @@ class Book {
 }
 
 class AdventureBook extends Book {
-    constructor(title, description, author) {
-        super(title, description, author)
-    }
+
     getAdventureBookInfo() {
-        return "Title: " + this.title + "; " + "description: " + this.description + ", " + "author: " + this.author;
+        return "Title: " + this.title + "; " + "description: " + this.description + "; " + "author: " + this.author + '.';
     }
 }
-let newBook = new AdventureBook('Titanik', 'trash', 'people');
-console.log(newBook.getAdventureBookInfo());
+
+console.log(new AdventureBook('StoryTales', 'for children', 'people').getAdventureBookInfo());
